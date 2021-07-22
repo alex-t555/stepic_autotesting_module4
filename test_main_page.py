bf4bff32cc84d353d57b758cc56883f1b2886c42
Alex.T <alex.rabota.555@gmail.com>
2021-07-22 17:48:07 +0300

    First test based on Page Object Pattern

--- test_main_page.py
+++ test_main_page.py
@@ -2,9 +2,13 @@
 """
 
 from selenium.webdriver.chrome.webdriver import WebDriver
+from .pages.main_page import MainPage
+
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser: WebDriver) -> None:
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    browser.find_element_by_css_selector('#login_link').click()
+    page = MainPage(browser=browser, url=link)
+    page.open()
+    page.go_to_login_page()
