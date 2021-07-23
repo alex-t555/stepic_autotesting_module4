f4d9bb82cf67a6676e84de5bbff0d66649e27463
Alex.T <alex.rabota.555@gmail.com>
2021-07-23 14:19:07 +0300

    Added LoginPage Object and locators for all Objects

--- test_main_page.py
+++ test_main_page.py
@@ -4,6 +4,7 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -13,6 +14,8 @@ def test_guest_can_go_to_login_page(browser: WebDriver) -> None:
     page = MainPage(browser=browser, url=link)
     page.open()
     page.go_to_login_page()
+    page = LoginPage(browser=browser, url=browser.current_url)
+    page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser: WebDriver) -> None:
