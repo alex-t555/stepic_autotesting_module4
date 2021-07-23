2672733624cf4243d49d2a6b3a01680707c3c9a9
Alex.T <alex.rabota.555@gmail.com>
2021-07-23 18:54:31 +0300

    Added parametrize product page

--- test_product_page.py
+++ test_product_page.py
@@ -1,15 +1,32 @@
 """ test_product_page.py
 """
 
+from inspect import ArgInfo
+from _pytest.mark import param
+from _pytest.outcomes import xfail
+import pytest
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 from .pages.product_page import ProductPage
 
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+        pytest.param(
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", 
+            marks=pytest.mark.xfail
+        ),
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 
-def test_guest_can_add_product_to_basket(browser: WebDriver) -> None:
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_basket(browser: WebDriver, link: str) -> None:
     page = ProductPage(browser=browser, url=link)
     page.open()
     page.add_product_to_basket()
